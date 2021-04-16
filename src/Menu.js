905d09e817f9ec6656e6a1f8a260bb80c213a238
pras75299 <singh.prashantking@gmail.com>
2021-04-16 22:20:52 +0530

    complete menu component

--- src/Menu.js
+++ src/Menu.js
@@ -9,8 +9,9 @@ const Menu = ({items}) => {
         <div className="item-info">
           <header>
             <h4>{title}</h4>
-            <h4 className="price">{price}</h4>
+            <h4 className="price">${price}</h4>
           </header>
+          <p className="item-text">{desc}</p>
         </div>
       </article>
       );
