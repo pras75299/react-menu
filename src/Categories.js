611c957ff5bc32c9611a270b15b2d419b2a49390
pras75299 <singh.prashantking@gmail.com>
2021-04-17 10:11:07 +0530

    create all category button for all menu items

--- src/Categories.js
+++ src/Categories.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Categories = ({filterItems}) => {
   return <div className="btn-container">
+    <button className="filter-btn" onClick={() => filterItems('all')}>All</button>
     <button className="filter-btn" onClick={() => filterItems('breakfast')}>Breakfast</button>
     <button className="filter-btn" onClick={() => filterItems('lunch')}>Lunch</button>
     <button className="filter-btn" onClick={() => filterItems('shakes')}>Shakes</button>
