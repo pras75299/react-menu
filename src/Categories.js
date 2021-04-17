29df2a94f929fc9fb04622858b1345d15b6aa7d7
pras75299 <singh.prashantking@gmail.com>
2021-04-17 11:15:14 +0530

    dynamic category button add in Categories component

--- src/Categories.js
+++ src/Categories.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const Categories = ({filterItems}) => {
-  return <div className="btn-container">
-    <button className="filter-btn" onClick={() => filterItems('all')}>All</button>
-    <button className="filter-btn" onClick={() => filterItems('breakfast')}>Breakfast</button>
-    <button className="filter-btn" onClick={() => filterItems('lunch')}>Lunch</button>
-    <button className="filter-btn" onClick={() => filterItems('shakes')}>Shakes</button>
-  </div>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
